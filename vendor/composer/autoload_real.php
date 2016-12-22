70f3e3c7189791bf1c068f7c436fbb76ed98865e
marcelo-idoso <marcelo_idoso@hotmail.com>
2016-12-21 23:03:56 -0200

    proplema com o git

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -27,7 +27,7 @@ class ComposerAutoloaderInitd34eafd3a0785f75ba5aa576ab4c596b
         array_push($includePaths, get_include_path());
         set_include_path(join(PATH_SEPARATOR, $includePaths));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
