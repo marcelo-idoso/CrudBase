10599f740ac4e537463d8e62abeac95203b6d46f
marcelo-idoso <marcelo_idoso@hotmail.com>
2016-12-22 00:25:49 -0200

    Inicio da Configuração Do Authorize

--- config/autoload/bjyauthorize.global.php
+++ config/autoload/bjyauthorize.global.php
@@ -1,39 +1,44 @@
 <?php
 
-use Zend\Permissions\Acl\Acl;
-use Zend\Permissions\Acl\Role\GenericRole as Role;
-use Zend\Permissions\Acl\Resource\GenericResource as Resource;
-
-$acl = new Acl();
-$acl->addRole(new Role('guest'))
-    ->addRole(new Role('admin'));
-
-$acl->addResource(new Resource('someResource'));
-$acl->addResource(new Resource('adminarea'));
-$acl->addResource(new Resource('adminconferencearea'));
-$acl->addResource(new Resource('adminsettingarea'));
-
-
-$acl->allow('guest', 'someResource');
-$acl->allow('admin', 'adminarea');
-$acl->allow('admin', 'adminconferencearea');
-$acl->allow('admin', 'adminsettingarea');
-
 return [
     'bjyauthorize' => [
         'default_role' => 'guest',
-        
         'identity_provider' => 'BjyAuthorize\Provider\Identity\AuthenticationIdentityProvider',
-        
         'role_providers' => [
+
+            'BjyAuthorize\Provider\Role\ObjectRepositoryProvider' => [
+                'object_manager' => 'doctrine.entitymanager.orm_default',
+                'role_entity_class' => 'Application\Entity\Role',
+            ],
             'BjyAuthorize\Provider\Role\Config' => [
                 'guest' => [],
                 'admin' => [],
             ]
         ],
+        'resource_providers' => [
+            'BjyAuthorize\Provider\Resource\Config' => [
+                'Module' => [],
+            ]
+        ],
+        'rule_providers' => [
+            'BjyAuthorize\Provider\Rule\Config' => [
+                'allow' => [
+                    [
+                        ['admin'], 'Module', ['edit'],
+                    ]
+                ]
+            ]
+        ],
         'guards' => [
             'BjyAuthorize\Guard\Controller' => [
-
+                [
+                    'controller' => 'zfcuser',
+                    'roles' => array('guest',)
+                ],
+                [
+                    'controller' => 'Application\Controller\Module',
+                    'roles' => array('admin')
+                ]
             ]
         ]
     ]
